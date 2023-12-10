459849b6218887b5d5f0574b9a66fb75e1a3570f
AnjaliBharwani <acksacks2405@gmail.com>
2023-12-10 23:24:35 +0530

    submitting code-Sun Dec 10 2023 23:24:34 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        let [name,setName] = useState("");
+
         <p>Enter your name:</p>
         <input type="text" onChange={(e)=> setName(e.target.value)}/>
 
